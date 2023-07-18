64bfb440bdda2118e9dfe9f083702690fb5487bc
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-07-18 10:52:58 +0000

    added new challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -21,6 +20,7 @@ class square():
     def __str__(self):
         """ return string format """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
